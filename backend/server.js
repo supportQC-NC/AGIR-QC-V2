a4625446f91a4c12ef7a61bcbf7d2456b3d3638f
supportQC-NC <supportQC-NC@users.noreply.github.com>
2026-03-30 12:59:24 +1100

    modifi server production

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.use("/api/filiales", filialeRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-  app.get("*", (req, res) =>
+  app.get("/{*splat}", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
